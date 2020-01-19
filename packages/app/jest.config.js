c6901dbb206ed1dc0521617cbc678b4418d057d9
Marcelo Morgado <cmarcelom@gmail.com>
2020-01-19 16:47:30 +0000

    Renaming jest setup file

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -11,15 +11,7 @@ module.exports = {
 
   snapshotSerializers: ["enzyme-to-json/serializer"],
 
-  setupFiles: ["<rootDir>/test-setup.js"],
-
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
-  // setupFilesAfterEnv: [
-  //   "@testing-library/react/cleanup-after-each",
-  //   "@testing-library/jest-dom/extend-expect"
-  // ],
+  setupFiles: ["<rootDir>/jest.setup.js"],
 
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
