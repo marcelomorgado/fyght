c201f68a2c7648dd731d1897baf9229ba42ea280
Marcelo Morgado <cmarcelom@gmail.com>
2020-01-19 15:38:59 +0000

    React+TS bootstraping

---
+++ packages/app/src/Counter.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Count from "./Count";
+
+export default class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Count count={this.state.count} />
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
