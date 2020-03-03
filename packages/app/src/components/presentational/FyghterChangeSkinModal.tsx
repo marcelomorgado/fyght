e9020aa390ac7c93259119d86c0c27943b48de51
Marcelo Morgado <cmarcelom@gmail.com>
2020-03-03 20:49:56 +0000

    Updating antd to 4.0.0

--- packages/app/src/components/presentational/FyghterChangeSkinModal.tsx
+++ packages/app/src/components/presentational/FyghterChangeSkinModal.tsx
@@ -1,85 +1,96 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useContext, useState } from "react";
 import { Button, Modal, Form, Radio } from "antd";
 import { skins } from "../../helpers";
 import { SkinAvatar } from "./SkinAvatar";
+import FyghtContext from "../../FyghtContext";
 
-// TODO: To use the correct type
-// See more: https://github.com/ant-design/ant-design/issues/19773#issuecomment-562487419
-const FyghterChangeSkinForm: any = Form.create({
-  name: "form_in_modal",
-})(
-  class extends React.Component<any> {
-    render() {
-      const { visible, onCancel, onCreate /*, onChange*/ } = this.props;
-      return (
-        <Modal
-          visible={visible}
-          title="Rename fyghter"
-          okText="Save"
-          onCancel={onCancel}
-          onOk={onCreate}
-        >
-          <Form layout="vertical">
-            <Form.Item label="Name">
-              <Radio.Group onChange={() => {}} value={undefined}>
-                {skins.map(({ skin }, i: number) => (
-                  <Radio key={i} value={skin}>
-                    <SkinAvatar size="small" skin={skin} />
-                  </Radio>
-                ))}
-              </Radio.Group>
-            </Form.Item>
-          </Form>
-        </Modal>
-      );
-    }
-  }
-);
-
-export class FyghterChangeSkinModal extends React.Component {
-  state = {
-    visible: false,
-  };
-  formRef: any;
-
-  showModal = () => {
-    this.setState({ visible: true });
-  };
-
-  handleCancel = () => {
-    this.setState({ visible: false });
-  };
-
-  handleCreate = () => {
-    const { form } = this.formRef.props;
-    form.validateFields((err: Error, values: any) => {
-      if (err) {
-        return;
-      }
-
-      console.log("Received values of form: ", values);
-      form.resetFields();
-      this.setState({ visible: false });
-    });
-  };
-
-  saveFormRef = (formRef: any) => {
-    this.formRef = formRef;
-  };
-
-  render() {
-    return (
-      <div>
-        <Button type="primary" block={true} onClick={this.showModal}>
-          Change skin
-        </Button>
-        <FyghterChangeSkinForm
-          wrappedComponentRef={this.saveFormRef}
-          visible={this.state.visible}
-          onCancel={this.handleCancel}
-          onCreate={this.handleCreate}
-        />
-      </div>
-    );
-  }
+interface Values {
+  skin: string;
 }
+
+interface FyghterChangeSkinFormProps {
+  visible: boolean;
+  // TODO: To use Values type above
+  onCreate: (values: any) => void;
+  onCancel: () => void;
+}
+
+const FyghterChangeSkinForm: React.FC<FyghterChangeSkinFormProps> = ({
+  visible,
+  onCancel,
+  onCreate,
+}) => {
+  const [form] = Form.useForm();
+
+  return (
+    <Modal
+      visible={visible}
+      title="Change skin"
+      okText="Save"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        // TODO: Get from state
+        initialValues={{ skin: "naked" }}
+      >
+        <Form.Item name="skin" label="">
+          <Radio.Group>
+            {skins.map(({ skin }, i: number) => (
+              <Radio key={i} value={skin}>
+                <SkinAvatar size="small" skin={skin} />
+              </Radio>
+            ))}
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export const FyghterChangeSkinModal = () => {
+  const [isVisible, setVisible] = useState(false);
+
+  //const { dispatch } = useContext(FyghtContext);
+
+  const onSave = (values: any) => {
+    console.log("Received values of form: ", values);
+    setVisible(false);
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        block={true}
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        Change skin
+      </Button>
+      <FyghterChangeSkinForm
+        visible={isVisible}
+        onCancel={() => {
+          setVisible(false);
+        }}
+        onCreate={onSave}
+      />
+    </div>
+  );
+};
--- packages/app/src/components/presentational/FyghterRenamingModal.tsx
+++ packages/app/src/components/presentational/FyghterRenamingModal.tsx
@@ -1,83 +1,119 @@
+/* eslint-disable react/prop-types */
 import React, { useContext, useState } from "react";
 import { Button, Modal, Form, Input } from "antd";
 import FyghtContext from "../../FyghtContext";
 
 // TODO: To use the correct type
 // See more: https://github.com/ant-design/ant-design/issues/19773#issuecomment-562487419
-const FyghterRenamingForm: any = Form.create({ name: "form_in_modal" })(
-  class extends React.Component<any> {
-    render() {
-      const { visible, onCancel, onCreate, form } = this.props;
-      const { getFieldDecorator } = form;
-      return (
-        <Modal
-          visible={visible}
-          title="Rename fyghter"
-          okText="Save"
-          onCancel={onCancel}
-          onOk={onCreate}
+// const FyghterRenamingForm: any = Form.create({ name: "form_in_modal" })(
+//   class extends React.Component<any> {
+//     render() {
+//       const { visible, onCancel, onCreate, form } = this.props;
+//       const { getFieldDecorator } = form;
+//       return (
+//         <Modal
+//           visible={visible}
+//           title="Rename fyghter"
+//           okText="Save"
+//           onCancel={onCancel}
+//           onOk={onCreate}
+//         >
+//           <Form layout="vertical">
+//             <Form.Item label="Name">
+//               {getFieldDecorator("name", {
+//                 rules: [
+//                   {
+//                     required: true,
+//                     message: "Please input the name of the fyghter!",
+//                   },
+//                 ],
+//               })(<Input />)}
+//             </Form.Item>
+//           </Form>
+//         </Modal>
+//       );
+//     }
+//   }
+// );
+
+interface Values {}
+
+interface FyghterRenamingFormProps {
+  visible: boolean;
+  // TODO: To use Values type above
+  onCreate: (values: any) => void;
+  onCancel: () => void;
+}
+
+const FyghterRenamingForm: React.FC<FyghterRenamingFormProps> = ({
+  visible,
+  onCancel,
+  onCreate,
+}) => {
+  const [form] = Form.useForm();
+  return (
+    <Modal
+      visible={visible}
+      title="Rename fyghter"
+      okText="Save"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch(info => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form form={form} layout="vertical" name="form_in_modal">
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+              message: "Please input the name of the fyghter!",
+            },
+          ]}
         >
-          <Form layout="vertical">
-            <Form.Item label="Name">
-              {getFieldDecorator("name", {
-                rules: [
-                  {
-                    required: true,
-                    message: "Please input the name of the fyghter!",
-                  },
-                ],
-              })(<Input />)}
-            </Form.Item>
-          </Form>
-        </Modal>
-      );
-    }
-  }
-);
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
 
 export const FyghterRenamingModal = () => {
   const [isVisible, setVisible] = useState(false);
-  const [formRef, setFormRef] = useState(null);
+
   const { dispatch } = useContext(FyghtContext);
 
-  const showModal = () => {
-    setVisible(true);
-  };
-
-  const handleCancel = () => {
+  const onSave = ({ name }: { name: string }) => {
+    dispatch({ type: "RENAME", payload: { name } });
     setVisible(false);
-  };
-
-  const handleCreate = () => {
-    const { form } = formRef.props;
-    form.validateFields((err: Error, values: any) => {
-      if (err) {
-        return;
-      }
-
-      const { name } = values;
-
-      dispatch({ type: "RENAME", payload: { name } });
-
-      form.resetFields();
-      setVisible(false);
-    });
-  };
-
-  const saveFormRef = (formRef: any) => {
-    setFormRef(formRef);
   };
 
   return (
     <div>
-      <Button type="primary" block={true} onClick={showModal}>
+      <Button
+        type="primary"
+        block={true}
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
         Rename
       </Button>
       <FyghterRenamingForm
-        wrappedComponentRef={saveFormRef}
         visible={isVisible}
-        onCancel={handleCancel}
-        onCreate={handleCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+        onCreate={onSave}
       />
     </div>
   );
