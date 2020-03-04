9a9ac546624ec45c02a4852e755339a8e9368e25
Marcelo Morgado <cmarcelom@gmail.com>
2020-03-04 22:06:12 +0000

    Removing unnused code

--- packages/app/src/testHelpers/probabilityTest.js
+++
@@ -1,7 +0,0 @@
-const p = (myXp, hisXp) => 1000 / (myXp + hisXp);
-
-for (let myXp = 1; myXp <= 100; ++myXp) {
-  for (let hisXp = 1; hisXp <= 100; ++hisXp) {
-    console.log(`myXp=${myXp}, hisXp=${hisXp}, p=${p(myXp, hisXp)}`);
-  }
-}
