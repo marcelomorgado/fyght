8ad6a35a2ca4af0f8941d2bda83a438e5be2a1ca
Marcelo Morgado <cmarcelom@gmail.com>
2020-03-05 21:09:22 +0000

    OpenZeppeling update and contracts DRY

--- packages/contracts/contracts/FighterFactory.sol
+++ packages/contracts/contracts/FighterFactory.sol
@@ -1,36 +1,34 @@
-pragma solidity 0.5.16;
+pragma solidity 0.6.1;
 
-import "./Ownable.sol";
-import "./SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract FighterFactory is Ownable {
+contract FighterFactory {
+    using SafeMath for uint256;
 
-  using SafeMath for uint256;
+    event NewFighter(uint256 fighterId, string name);
 
-  event NewFighter(uint fighterId, string name);
+    struct Fighter {
+        uint256 id;
+        string name;
+        string skin;
+        uint32 xp;
+        uint32 qi;
+        uint16 winCount;
+        uint16 lossCount;
+    }
 
-  struct Fighter {
-    uint id;
-    string name;
-    string skin;
-    uint32 xp;
-    uint32 qi;
-    uint16 winCount;
-    uint16 lossCount;
-  }
+    Fighter[] public fighters;
 
-  Fighter[] public fighters;
+    mapping(uint256 => address) public fighterToOwner;
+    mapping(address => uint256) ownerFighterCount;
 
-  mapping (uint => address) public fighterToOwner;
-  mapping (address => uint) ownerFighterCount;
-
-  function createFighter(string memory _name) public {
-    require(ownerFighterCount[msg.sender] == 0);
-    uint id = fighters.length;
-    fighters.push(Fighter(id, _name, 'naked', 1, 1, 0, 0));
-    fighterToOwner[id] = msg.sender;
-    ownerFighterCount[msg.sender]++;
-    emit NewFighter(id, _name);
-  }
+    function createFighter(string memory _name) public {
+        require(ownerFighterCount[msg.sender] == 0);
+        uint256 id = fighters.length;
+        fighters.push(Fighter(id, _name, "naked", 1, 1, 0, 0));
+        fighterToOwner[id] = msg.sender;
+        ownerFighterCount[msg.sender]++;
+        emit NewFighter(id, _name);
+    }
 
 }
--- packages/contracts/contracts/FighterHelper.sol
+++ packages/contracts/contracts/FighterHelper.sol
@@ -1,98 +1,86 @@
-pragma solidity 0.5.16;
+pragma solidity 0.6.1;
 
 import "./FighterFactory.sol";
 
 contract FighterHelper is FighterFactory {
+    uint256 public trainingFee = 0.005 ether;
 
-  uint public trainingFee = 0.005 ether;
-
-  modifier onlyOwnerOf(uint _fighterId) {
-    require(msg.sender == fighterToOwner[_fighterId]);
-    _;
-  }
-
-  function withdraw() external onlyOwner {
-    owner.transfer(address(this).balance);
-  }
-
-  function _checkForSkinUpdate(uint _fighterId) internal {
-    Fighter storage fighter = fighters[_fighterId];
-    if(fighter.xp == 10) {
-      fighter.skin = 'normal_guy';
+    modifier onlyOwnerOf(uint256 _fighterId) {
+        require(msg.sender == fighterToOwner[_fighterId]);
+        _;
     }
-    else if(fighter.xp == 15) {
-      fighter.skin = 'karate_kid';
+
+    function _checkForSkinUpdate(uint256 _fighterId) internal {
+        Fighter storage fighter = fighters[_fighterId];
+        if (fighter.xp == 10) {
+            fighter.skin = "normal_guy";
+        } else if (fighter.xp == 15) {
+            fighter.skin = "karate_kid";
+        } else if (fighter.xp == 25) {
+            fighter.skin = "japonese";
+        } else if ((fighter.xp == 30 && fighter.qi >= 30) || fighter.xp == 40) {
+            fighter.skin = "monk";
+        } else if (fighter.xp == 50) {
+            fighter.skin = "ninja";
+        } else if (fighter.xp == 80) {
+            fighter.skin = "no_one";
+        } else if (fighter.xp == 100 && fighter.qi == 100) {
+            fighter.skin = "master";
+        }
     }
-    else if(fighter.xp == 25) {
-      fighter.skin = 'japonese';
+
+    function changeSkin(uint256 _fighterId, string calldata _newSkin) external {
+        require(fighters[_fighterId].xp >= 80);
+
+        bytes32 newSkinHash = keccak256(abi.encodePacked(_newSkin));
+        require(
+            newSkinHash == keccak256("naked") ||
+                newSkinHash == keccak256("normal_guy") ||
+                newSkinHash == keccak256("karate_kid") ||
+                newSkinHash == keccak256("japonese") ||
+                newSkinHash == keccak256("monk") ||
+                newSkinHash == keccak256("ninja") ||
+                newSkinHash == keccak256("no_one") ||
+                newSkinHash == keccak256("daemon") ||
+                newSkinHash == keccak256("master")
+        );
+
+        if (newSkinHash == keccak256("master")) {
+            require(fighters[_fighterId].xp >= 100 && fighters[_fighterId].qi >= 100);
+        }
+
+        fighters[_fighterId].skin = _newSkin;
     }
-    else if(fighter.xp == 30 && fighter.qi >= 30 || fighter.xp == 40) {
-      fighter.skin = 'monk';
+
+    function setXpTrainingFee(uint256 _fee) external {
+        trainingFee = _fee;
     }
-    else if(fighter.xp == 50) {
-      fighter.skin = 'ninja';
+
+    function training(uint256 _fighterId) external payable {
+        uint256 price = fighters[_fighterId].qi * trainingFee;
+        require(msg.value == price);
+        fighters[_fighterId].qi++;
+        _checkForSkinUpdate(_fighterId);
     }
-    else if(fighter.xp == 80) {
-      fighter.skin = 'no_one';
+
+    function renameFighter(uint256 _fighterId, string calldata _newName) external onlyOwnerOf(_fighterId) {
+        fighters[_fighterId].name = _newName;
     }
-    else if(fighter.xp == 100 && fighter.qi == 100) {
-      fighter.skin = 'master';
+
+    function getFightersByOwner(address _owner) external view returns (uint256[] memory) {
+        uint256[] memory result = new uint256[](ownerFighterCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < fighters.length; i++) {
+            if (fighterToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
     }
-  }
 
-  function changeSkin(uint _fighterId, string calldata _newSkin) external {
-    require(fighters[_fighterId].xp >= 80);
-
-    bytes32 newSkinHash = keccak256(abi.encodePacked(_newSkin));
-    require(
-      newSkinHash == keccak256('naked') ||
-      newSkinHash == keccak256('normal_guy') ||
-      newSkinHash == keccak256('karate_kid') ||
-      newSkinHash == keccak256('japonese') ||
-      newSkinHash == keccak256('monk') ||
-      newSkinHash == keccak256('ninja') ||
-      newSkinHash == keccak256('no_one') ||
-      newSkinHash == keccak256('daemon') ||
-      newSkinHash == keccak256('master')
-    );
-
-    if(newSkinHash == keccak256('master')) {
-      require(fighters[_fighterId].xp >= 100 && fighters[_fighterId].qi >= 100);
+    function getFightersCount() external view returns (uint256) {
+        return fighters.length;
     }
-    
-    fighters[_fighterId].skin = _newSkin;
-  }
-
-  function setXpTrainingFee(uint _fee) external onlyOwner {
-    trainingFee = _fee;
-  }
-
-  function training(uint _fighterId) external payable {
-    uint price = fighters[_fighterId].qi * trainingFee;
-    require(msg.value == price);
-    fighters[_fighterId].qi++;
-    _checkForSkinUpdate(_fighterId);
-  }
-
-  function renameFighter(uint _fighterId, string calldata _newName) external onlyOwnerOf(_fighterId) {
-    fighters[_fighterId].name = _newName;
-  }
-
-  function getFightersByOwner(address _owner) external view returns(uint[] memory) {
-    uint[] memory result = new uint[](ownerFighterCount[_owner]);
-    uint counter = 0;
-    for (uint i = 0; i < fighters.length; i++) {
-      if (fighterToOwner[i] == _owner) {
-        result[counter] = i;
-        counter++;
-      }
-    }
-    return result;
-  }
-
-  function getFightersCount() external view returns(uint) {
-    return fighters.length;
-  }
-
 
 }
