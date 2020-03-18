d851fb7af2ded64abee4862d55d712bc94c3bc74
Marcelo Morgado <cmarcelom@gmail.com>
2020-03-18 20:59:22 +0000

    Creation on deposit fix

--- packages/contracts/contracts/Dai.sol
+++ packages/contracts/contracts/Dai.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Dai is ERC20 {
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
     }
 }
