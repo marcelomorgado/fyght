182737e6fcfed520262789235082a660c403fa5d
Marcelo Morgado <cmarcelom@gmail.com>
2020-04-11 14:18:48 +0100

    Refactoring

--- packages/contracts/contracts/EthereumDai.sol
+++ packages/contracts/contracts/EthereumDai.sol
@@ -4,11 +4,10 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-
 contract EthereumDai is ERC20, ERC20Detailed {
-    constructor() public payable ERC20Detailed("Dai", "DAI", 18) {}
+    constructor() public ERC20Detailed("Dai", "DAI", 18) {}
 
-    function mint(uint256 amount) public {
-        _mint(msg.sender, amount);
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
     }
 }
