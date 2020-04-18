774780969acd3f100b3aadfbeee87f7e075533fb
Marcelo Morgado <cmarcelom@gmail.com>
2020-04-18 15:21:04 +0100

    Tests upgrade

--- packages/app/src/components/presentational/DepositButton/DepositButton.spec.tsx
+++ packages/app/src/components/presentational/DepositButton/DepositButton.spec.tsx
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import { DepositButton } from "./DepositButton";
 import * as FyghtState from "../../../state";
 import { storeMocks } from "../../../testHelpers";
+import { BigNumber } from "ethers/utils";
 
 describe("DepositButton", () => {
   const {
@@ -12,7 +13,14 @@ describe("DepositButton", () => {
   jest
     .spyOn(FyghtState, "useFyghtState")
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .mockImplementation((): any => [{}, { doDeposit: jest.fn() }]);
+    .mockImplementation((): any => [
+      {
+        metamask: {
+          contracts: { fyghters: { getMinimumDeposit: jest.fn(() => new BigNumber("5")) } },
+        },
+      },
+      { doDeposit: jest.fn() },
+    ]);
 
   test("should render the component", () => {
     expect(shallow(<DepositButton fyghterId={fyghterId} isLoading={false} />)).toMatchSnapshot();
